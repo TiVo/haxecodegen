a7622d16f725644c79cf4aaac12b6f128f975caa
Bryan Ischo <bryan@ischo.com>
2015-09-01 14:03:32 -0700

    Don't try to set properties on unwriteable fields.

--- GenStatementHelpers.hx
+++ GenStatementHelpers.hx
@@ -386,8 +386,9 @@ class GenStatementHelpers
             if (v == null) {
                 var s = Util.randomStatic(null);
                 if ((s != null) && s.field.isWriteable()) {
-                    // If it's dynamic, do a dynamic property set
-                    if (s.field.type == GenTypeDynamic) {
+                    // If it's dynamic and writable, do a dynamic property set
+                    if (s.field.isWriteable() &&
+                        (s.field.type == GenTypeDynamic)) {
                         return Assignment(s.gc.fullname + "." + s.field.name +
                                           "." + Random.identifier(false),
                                           randomExpressionOfType
